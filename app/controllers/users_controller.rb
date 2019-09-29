ceabcd58d25a91bf8dc30c3e5dfd95bec16ad36b
kotakosu <11920513k>
2019-09-29 15:47:06 +0900

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
-    @users =User.where.not(id: current_user.id)
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
     
     respond_to do |format|
       format.html { redirect_to new_proup_path}
